205128115132c1b32bc033d0f4bde1d362255799
marshy <marshy@zeta.com.au>
2016-02-05 13:44:36 +0800

    accept goodreads id rather than rss url, wrap images in an anchor

--- Web/scripts/app.js
+++ Web/scripts/app.js
@@ -2,26 +2,47 @@
 
 $(function () {
 
-    var getBooks = function (readRSS) {
-        var url = 'https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=\"' + encodeURIComponent(readRSS) + '\"&format=json';
-        $.get(url, function (data) {
-            //console.log(data.query.results.item);
-            $(data.query.results.item).each(function (index) {
-                console.log(this.book_large_image_url);
-                var img = document.createElement("img");
-                img.setAttribute("src", this.book_large_image_url);
-                img.setAttribute("alt", this.title);
-                $("#results").append(img);
-            });
+    var renderResults = function (results) {
+        console.log(results);
+        $(results.item).each(function (index) {
+            var img = document.createElement("img");
+            img.setAttribute("src", this.book_large_image_url);
+            img.setAttribute("alt", this.title);
+            var a = document.createElement("a");
+            a.setAttribute("href", "https://www.goodreads.com/book/show/" + this.book_id);
+            a.setAttribute("target", "_blank");
+            a.setAttribute("title", "Go to this book on Goodreads");
+            $(a).append(img)
+            $("#results").append(a);
         });
+    }
 
+    var getBooks = function (goodreadsId) {
+        var goodreadsRead = JSON.parse(localStorage.getItem("goodreadsRead" + goodreadsId));
+        if (goodreadsRead) {
+            renderResults(goodreadsRead);
+        }
+        else {
+            var readRSS = "https://www.goodreads.com/review/list_rss/" + goodreadsId + "?shelf=read";
+            var url = 'https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=\"' + encodeURIComponent(readRSS) + '\"&format=json';
+            $.get(url, function (data) {
+                if (data.query.results) {
+                    var results = data.query.results;
+                    localStorage.setItem("goodreadsRead" + goodreadsId, JSON.stringify(results));
+                    renderResults(results);
+                } else {
+                    alert("Sorry, couldn't find anything.  Check your goodreads Id")
+                }
+            });
+        }
     }
 
     $("#frmRead").submit(function (event) {
         if (typeof (Storage) !== "undefined") {
-            var readRSS = $("#txtRead").val();
-            localStorage.setItem("readRSS", readRSS);
-            getBooks(readRSS);
+            var goodreadsId = $("#txtId").val();
+            localStorage.setItem("goodreadsId", goodreadsId);
+            localStorage.removeItem("goodreadsRead" + goodreadsId);
+            getBooks(goodreadsId);
         }
         else {
             alert("Sorry, your browser doesn't support local storage and therefore we can't help you.")
@@ -29,9 +50,14 @@ $(function () {
         event.preventDefault();
     })
 
-    var readRSS = localStorage.getItem("readRSS");
-    if (readRSS) {
-        getBooks(readRSS);
+    var goodreadsId = localStorage.getItem("goodreadsId");
+    if (goodreadsId) {
+        $("#txtId").val(goodreadsId);
+        getBooks(goodreadsId);
+    }
+    else {
+        alert("Enter your Goodreads Id");
+        $("#txtId").focus();
     };
 
 });
