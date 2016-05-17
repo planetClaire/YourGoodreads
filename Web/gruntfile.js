27dba2b7774280759387f638bbf75720285af27f
marshy <marshy@zeta.com.au>
2016-05-17 11:41:56 +0800

    upgrade to foundation 6.2.1 and switch from gulp to grunt

---
+++ Web/gruntfile.js
@@ -0,0 +1,120 @@
+﻿module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt); // load all tasks
+
+    var jsFoundation = [
+
+      //'bower_components/foundation-sites/js/foundation.core.js',
+      //'bower_components/foundation-sites/js/foundation.util.timerAndImageLoader.js',
+      //'bower_components/foundation-sites/js/foundation.util.keyboard.js',
+      //'bower_components/foundation-sites/js/foundation.util.mediaQuery.js',
+      //'bower_components/foundation-sites/js/foundation.util.motion.js',
+      //'bower_components/foundation-sites/js/foundation.util.triggers.js',
+      //'bower_components/foundation-sites/js/foundation.util.touch.js',
+      //'bower_components/foundation-sites/js/foundation.reveal.js',
+      //'bower_components/foundation-sites/js/foundation.abide.js',
+      //'bower_components/foundation-sites/js/foundation.accordion.js',
+      //'bower_components/foundation-sites/js/foundation.accordionMenu.js',
+      //'bower_components/foundation-sites/js/foundation.drilldown.js',
+      //'bower_components/foundation-sites/js/foundation.dropdown.js',
+      //'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
+      //'bower_components/foundation-sites/js/foundation.equalizer.js',
+      //'bower_components/foundation-sites/js/foundation.interchange.js',
+      //'bower_components/foundation-sites/js/foundation.magellan.js',
+      //'bower_components/foundation-sites/js/foundation.offcanvas.js',
+      //'bower_components/foundation-sites/js/foundation.orbit.js',
+      //'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
+      //'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
+      //'bower_components/foundation-sites/js/foundation.slider.js',
+      //'bower_components/foundation-sites/js/foundation.sticky.js',
+      //'bower_components/foundation-sites/js/foundation.tabs.js',
+      //'bower_components/foundation-sites/js/foundation.toggler.js',
+      //'bower_components/foundation-sites/js/foundation.tooltip.js',
+      //'bower_components/foundation-sites/js/foundation.util.box.js',
+      //'bower_components/foundation-sites/js/foundation.util.nest.js',
+
+    ];
+
+    var jsApp = [
+      'scripts/app.js',
+      'scripts/_*.js'
+    ];
+
+    var jsAll = [
+        [jsFoundation],
+        [jsApp]
+    ];
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed',
+                includePaths: ['bower_components/foundation-sites/scss', 'node_modules/motion-ui/src']
+            },
+            dist: {
+                files: {
+                    'styles/styles.css': 'scss/styles.scss'
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [jsAll],
+                dest: 'scripts/concat/scripts.js'
+            }
+        },
+
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['es2015']
+            },
+            dist: {
+                files: {
+                    'scripts/babel/scripts.js': 'scripts/concat/scripts.js'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'scripts/scripts.min.js': 'scripts/babel/scripts.js'
+                }
+            }
+        },
+
+        watch: {
+            grunt: {
+                files: ['gruntfile.js'],
+                tasks: ['build']
+            },
+            sass: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            },
+            js: {
+                files: [
+                    jsAll
+                ],
+                tasks: ['concat', 'babel', 'uglify']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('build', ['concat', 'babel', 'uglify', 'sass']);
+    grunt.registerTask('default', ['build', 'watch']);
+};
--- Web/scripts/app.js
+++ Web/scripts/app.js
@@ -1,63 +1,187 @@
-﻿"use strict";
+﻿//$(document).foundation();
 
 $(function () {
 
-    var renderResults = function (results) {
-        console.log(results);
-        $(results.item).each(function (index) {
+    var initTooltips = function () {
+        $('#results').imagesLoaded(function () {
+            $('.hasTooltip').each(function () {
+                $(this).qtip({
+                    content: {
+                        text: $(this).next('div'),
+                        button: true
+                    },
+                    position: {
+                        my: 'top left',
+                        at: 'center center'
+                    },
+                    show: 'click',
+                    hide: 'click',
+                    style: {
+                        classes: 'qtip-gr'
+                    }
+                });
+            });
+        });
+    };
+
+    var renderResults = function (results, goodreadsId) {
+        initTooltips();
+        $("#results").empty();
+        $(results).each(function () {
             var img = document.createElement("img");
             img.setAttribute("src", this.book_large_image_url);
             img.setAttribute("alt", this.title);
-            var a = document.createElement("a");
-            a.setAttribute("href", "https://www.goodreads.com/book/show/" + this.book_id);
-            a.setAttribute("target", "_blank");
-            a.setAttribute("title", "Go to this book on Goodreads");
-            $(a).append(img)
-            $("#results").append(a);
+            var li = document.createElement("li");
+            li.setAttribute("class", "book hasTooltip");
+            var span = document.createElement("span");
+            span.setAttribute("class", "img-helper");
+            $(li).append(span).append(img);
+            $("#results").append(li);
+            var hidden = document.createElement("div");
+            hidden.setAttribute("class", "hidden");
+            var title = document.createElement("h2");
+            $(title).html("<a href='https://www.goodreads.com/book/show/" + this.book_id + "' target='_blank'>" + this.title + "</a>");
+            var author = document.createElement("h3");
+            $(author).html("by " + this.author_name);
+            var published = document.createElement("h4");
+            $(published).html(this.book.num_pages + " pages | Published " + this.book_published);
+            var read = document.createElement("h5");
+            var readDate = Date.parse(this.user_read_at);
+            if (readDate) {
+                $(read).html("<a href='https://www.goodreads.com/review/list/" + goodreadsId + "?shelf=read' + target='_blank'>Finished " + new Date(this.user_read_at).toDateString() + "</a>");
+            } else {
+                $(read).html("<a href='https://www.goodreads.com/review/list/" + goodreadsId + "?shelf=currently-reading' + target='_blank'>Currently reading</a>");
+            }
+            var rating = document.createElement("span");
+            rating.setAttribute("class", "rating");
+            rating.setAttribute("title", "Your rating");
+            var userStars = this.user_rating;
+            for (var i = 0; i < 5; i++) {
+                if (userStars > i) {
+                    $(rating).append("&#9733;");
+                } else {
+                    $(rating).append("&#9734;");
+                }
+            }
+            var averageRating = document.createElement("span");
+            averageRating.setAttribute("class", "averageRating");
+            averageRating.setAttribute("title", "Average rating " + this.average_rating);
+            var averageStars = Math.round(this.average_rating);
+            for (var i = 0; i < 5; i++) {
+                if (averageStars > i) {
+                    $(averageRating).append("&#9733;");
+                } else {
+                    $(averageRating).append("&#9734;");
+                }
+            }
+            var description = document.createElement("p");
+            description.setAttribute("class", "description");
+            $(description).html(this.book_description);
+            $(hidden).append(title).append(author).append(published).append(read).append(rating).append(averageRating).append(description);
+            $("#results").append(hidden);
+            //initTooltips();
         });
     }
 
-    var getBooks = function (goodreadsId) {
-        var goodreadsRead = JSON.parse(localStorage.getItem("goodreadsRead" + goodreadsId));
+    var getBooks = function (goodreadsId, refresh) {
+        var goodreadsRead = refresh ? null : JSON.parse(localStorage.getItem("goodreadsRead" + goodreadsId));
         if (goodreadsRead) {
-            renderResults(goodreadsRead);
+            renderResults(goodreadsRead, goodreadsId);
         }
         else {
-            var readRSS = "https://www.goodreads.com/review/list_rss/" + goodreadsId + "?shelf=read";
-            var url = 'https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=\"' + encodeURIComponent(readRSS) + '\"&format=json';
-            $.get(url, function (data) {
-                if (data.query.results) {
-                    var results = data.query.results;
-                    localStorage.setItem("goodreadsRead" + goodreadsId, JSON.stringify(results));
-                    renderResults(results);
+            var readRss = "https://www.goodreads.com/review/list_rss/" + goodreadsId + "?shelf=read";
+            var readUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=\"' + encodeURIComponent(readRss) + '\"&format=json';
+            $("#overlay").show();
+            $.get(readUrl, function (readData) {
+                if (readData.query.results) {
+                    var results = readData.query.results.item;
+                    var currentlyReadingRss = "https://www.goodreads.com/review/list_rss/" + goodreadsId + "?shelf=currently-reading";
+                    var currentlyReadingUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from rss where url=\"' + encodeURIComponent(currentlyReadingRss) + '\"&format=json';
+                    $.get(currentlyReadingUrl, function (currentlyReadingData) {
+                        if (currentlyReadingData.query.results) {
+                            results = currentlyReadingData.query.results.item.concat(results);
+                        }
+                        var goodreadsPref = localStorage.getItem("goodreadsPref");
+                        if (goodreadsPref === "ThisYear") {
+                            var thisYear = new Date().getFullYear();
+                            results = $.grep(results, function(b) {
+                                return (b.user_shelves && b.user_shelves.contains("currently-reading")) || (b.user_read_at && b.user_read_at.contains(thisYear));
+                            });
+                        }
+                        localStorage.setItem("goodreadsRead" + goodreadsId, JSON.stringify(results));
+                        renderResults(results, goodreadsId);
+                        $("#overlay").hide();
+                    });
                 } else {
-                    alert("Sorry, couldn't find anything.  Check your goodreads Id")
+                    alert("Sorry, couldn't find anything. Check your goodreads Id");
                 }
             });
         }
     }
 
-    $("#frmRead").submit(function (event) {
+    var init = function (refresh) {
+        var goodreadsId = localStorage.getItem("goodreadsId");
+        if (goodreadsId) {
+            $("#txtId").val(goodreadsId);
+            $("#btnSubmit").text("Refresh");
+            getBooks(goodreadsId, refresh);
+        } else {
+            $("#btnSubmit").text("Go");
+            alert("Enter your Goodreads Id");
+            $("#txtId").focus();
+        };
+    };
+
+    $("#frmRead").submit(function(event) {
+        $('.qtip:visible').qtip("hide");
         if (typeof (Storage) !== "undefined") {
             var goodreadsId = $("#txtId").val();
             localStorage.setItem("goodreadsId", goodreadsId);
             localStorage.removeItem("goodreadsRead" + goodreadsId);
             getBooks(goodreadsId);
-        }
-        else {
-            alert("Sorry, your browser doesn't support local storage and therefore we can't help you.")
+        } else {
+            alert("Sorry, your browser doesn't support local storage and therefore we can't help you.");
         }
         event.preventDefault();
-    })
+    });
 
-    var goodreadsId = localStorage.getItem("goodreadsId");
-    if (goodreadsId) {
-        $("#txtId").val(goodreadsId);
-        getBooks(goodreadsId);
+    $("#aAll").click(function () {
+        $('.qtip:visible').qtip("hide");
+        var goodreadsPref = localStorage.getItem("goodreadsPref");
+        if (goodreadsPref && goodreadsPref !== "All") {
+            localStorage.setItem("goodreadsPref", "All");
+            $("#aAll").addClass("active");
+            $("#aThisYear").removeClass("active");
+            init(true);
+        }
+        return false;
+    });
+
+    $("#aThisYear").click(function () {
+        $('.qtip:visible').qtip("hide");
+        var goodreadsPref = localStorage.getItem("goodreadsPref");
+        if (goodreadsPref && goodreadsPref !== "ThisYear") {
+            localStorage.setItem("goodreadsPref", "ThisYear");
+            $("#aThisYear").addClass("active");
+            $("#aAll").removeClass("active");
+            init(true);
+        }
+        return false;
+    });
+
+
+    var goodreadsPref = localStorage.getItem("goodreadsPref");
+    if (!goodreadsPref) {
+        goodreadsPref = "All";
+        localStorage.setItem("goodreadsPref", goodreadsPref);
     }
-    else {
-        alert("Enter your Goodreads Id");
-        $("#txtId").focus();
-    };
+    if (goodreadsPref === "All") {
+        $("#aAll").addClass("active");
+    }
+    else if (goodreadsPref === "ThisYear") {
+        $("#aThisYear").addClass("active");
+    }
 
+    initTooltips();
+    init();
 });
